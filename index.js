9aa5ec7f20ca952754147db96201cb4a9320e926
Dugmar <dugmar.morocho@gmail.com>
2019-12-10 23:09:35 -0500

    results added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded",(e)=>{
+
+document.addEventListener("DOMContentLoaded",()=>{
 
     let div1 = document.querySelector("#Pokemon1") 
     let div2 = document.querySelector("#Pokemon2")
+    let pokemonArr = []
 
 const addChara = async (div) => {
 
@@ -21,7 +23,9 @@ const addChara = async (div) => {
         let name = data.name
         let h3Name = document.createElement("h3")
         h3Name.innerText = name
+        h3Name.id = name
         div.appendChild(h3Name)
+        pokemonArr.push(name)
         //IMAGE
         let src = data.sprites.front_default
         img.src=src
@@ -40,10 +44,11 @@ const addChara = async (div) => {
         for(let i = 0; i <=4; i ++){
 
             let randNum = Math.floor(Math.random() * (moves.length-1))
-
             let moveName = moves[randNum].move.name
+
             let moveData = await axios.get(moves[randNum].move.url)
             let movePP = moveData.data.pp
+
             let li = document.createElement("li")
             li.innerText = `${moveName}:  ${movePP}`
             ul.appendChild(li)
@@ -59,8 +64,10 @@ const addChara = async (div) => {
 
        
     }
+
 }
 
+//Get Pokemon and Display
 let getbtn = document.querySelector("#GetPokemon")
 getbtn.addEventListener("click",()=>{
 
@@ -68,6 +75,32 @@ addChara(div1)
 addChara(div2)
 
 })
+const battleFunction = () =>{
+let winner = pokemonArr.splice(Math.floor(Math.random()*2), 1).toString();
+let loser = pokemonArr[0]
+let h3 = document.querySelector("#results")
+h3.innerText = `The winner is ... ${winner}`
+let div = document.querySelector("#battle")
+let li = document.createElement("li")
+li.innerText = `${winner} beat ${loser}`
+
+pokemonArr = []
+
+}
+
+
+
+let battlebtn = document.querySelector("#Battle")
+
+battlebtn.addEventListener("click",(e)=>{
+
+
+    battleFunction(div1,div2);
+
+   
+    
+})
+
 
 
 })
